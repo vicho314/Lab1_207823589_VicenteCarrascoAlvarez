26c2d60457b5cde405c227842a5421505efc4e06
vicho314 <vicente.carrasco@usach.cl>
2024-10-01 12:58:14 -0300

    Definir TDA player y actualizar README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # lab_scheme
-Conecta4 en racket
+Conecta4 en racket, con integración con Google AI
+
+## Cómo ejecutar
+
+Usando la última version de DrRacket, correr los scripts en orden:
+
+FIXME: añadir scripts!
+
+## Progreso
+
+- [_] TDA Player 
+- [_] TDA Board
+- [_] TDA Game
+- [_] Integración con Google
+- [_] Scripts de prueba
+- [_] Informe
+
--- player.rkt
+++ player.rkt
@@ -7,5 +7,60 @@
 )
 
 ; Selectores
+(define (player-id pl)
+	(list-ref pl 1)
+)
+
+(define (player-name pl)
+	(list-ref pl 2)
+)
+
+(define (player-color pl)
+	(list-ref pl 3)
+)
+
+(define (player-wins pl)
+	(list-ref pl 4)
+)
+
+(define (player-losses pl)
+	(list-ref pl 5)
+)
+
+(define (player-draws pl)
+	(list-ref pl 6)
+)
+
+(define (player-rem_p pl)
+	(list-ref pl 7)
+)
 
 ; Modificadores
+(define (player-id-set pl id)
+	(list-set! pl 1 id)
+)
+
+(define (player-name-set pl name)
+	(list-set! pl 2 name)
+)
+
+(define (player-color-set pl color)
+	(list-set! pl 3 color)
+)
+
+(define (player-wins-set pl wins)
+	(list-set pl 4 wins)
+)
+
+(define (player-losses-set pl losses)
+	(list-set! pl 5 losses)
+)
+
+(define (player-draws-set pl draws)
+	(list-set! pl 6 draws)
+)
+
+(define (player-rem_p-set pl rem_pieces)
+	(list-set! pl 7 rem_pieces)
+)
+
