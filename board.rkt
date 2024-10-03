b7c21b27c2af1e083dd1d12e5065effe206a004c
vicho314 <vicente.carrasco@usach.cl>
2024-10-02 23:22:14 -0300

    TDA Board: Cambio a listas enlazadas

--- board.rkt
+++ board.rkt
@@ -18,37 +18,53 @@
 ; TDA Board
 ; Dom: void
 ; Matriz de 7x6 piezas vacías
-(define (do-filas n)
+(define (do-cols n)
 	(make-list n null)
 )
 
-(define (do-cols n lst)
-	(map (lambda (a) (do-filas n)) lst)
+(define (do-filas n lst)
+	(map (lambda (a) (do-cols n)) lst)
 )
-; Â[y][x] para tener rep. vertical
-(define board
-	(do-cols 7 (do-filas 6))
+; Â[x][y]
+(define (board)
+; USAR LISTAS ENLAZADAS, NO MATRICES!!!
+;	(do-filas 6 (do-cols 7))
+	(do-cols 7)
 )
 
+(define empty-board (board))
+
 ; Selectores
-(define (board-fila br y)
-	(list-ref br y)
+(define (board-col br x)
+	(list-ref br x)
 )
 
 ;FIXME: Usar numeración desde 0!!!
 (define (board-xy br x y)
-	(list-ref (board-fila br y) x)
+	(if	(< (length (board-col br x)) y)
+		null
+		(list-ref (board-col br x) y)
+	)
 )
 
 ; Modificadores
-; Modifica una fila
-(define (fila-set! br x y pieza)
-	(list-set! (board-fila br y) x pieza)
+; Modifica una columna
+(define (col-set br x y pieza)
+	(list-set (board-col br x) y pieza)
 )
 
 ; Modifica y ASIGNA una fila para modificar UNA PIEZA
-(define (board-set! br x y pieza)
-	(list-set! br y (fila-set! br x y pieza))
+(define (board-set br x y pieza)
+	(list-set br x (col-set br x y pieza))
+)
+
+(define (col-append br x pieza)
+	(append (board-col br x) pieza)
+)
+
+; Modifica y ASIGNA una fila para agregar UNA PIEZA
+(define (board-append br x pieza)
+	(list-set br x (col-append br x pieza))
 )
 
 ; Funciones
@@ -57,5 +73,22 @@
 	(display (reverse br))
 )
 
+(define (col-llena? col)
+	(= 6 (length col))
+)
+
+(define (board-can-play br)
+	(null? (filter col-llena? br))
+)
+
+; Dom: board (board) X column (int) X piece (piece)
+; Nota: Si col está llena, mostrar error y no realizar
+; jugada.
+(define (board-set-play-piece br col pieza)
+	(if (col-llena? (board-col br col))
+		(car (cons br (display "Error: col llena\n")))
+		(board-append br col pieza)
+	)
+)
 ; ################## TDA Board
 ( provide (all-defined-out))
