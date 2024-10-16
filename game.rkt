f850c146b59f20f6b0936122490616f6383f9c33
vicho314 <vicente.carrasco@usach.cl>
2024-10-16 15:34:36 -0300

    TDA game

--- game.rkt
+++ game.rkt
@@ -34,7 +34,47 @@
 	(list-ref 4 gm)
 )
 ; Modificadores
+(define (game-p1-set gm pl)
+	(list-set gm 0 pl)
+)
 
+
+(define (game-p2-set gm pl)
+	(list-set gm 1 pl)
+)
+
+
+(define (game-board-set gm br)
+	(list-set gm 2 br)
+)
+
+
+(define (game-turn-set gm turn)
+	(list-set gm 3 turn)
+)
+
+(define (game-history-set gm st)
+	(list-set gm 4 st)
+)
 ; Funciones
+(define (game-display-history gm)
+	(display (game-history))
+)
+
+(define (game-add-history gm col color)
+	(game-history-set gm 
+		(stack-push (game-history gm) (cons col color))
+	)
+)
+
+;FIXME: revisar casos borde o condiciones?
+(define (game-is-draw? gm)
+	(and (board-lleno? (game-board gm))
+		(or (player-has-pieces? (game-p1 gm))
+			(player-has-pieces? (game-p2 gm))
+		)
+	)
+)
+
 
 (all-defined-out)
